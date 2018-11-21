908d29806e3084128d9686e05bdbee50b5cd0d83
VvanGemert <vincent@floorplanner.com>
2018-11-21 17:07:59 +0100

    Used JSON for exchange rates instead of XML

--- src/europe/currency/exchange_rates.cr
+++ src/europe/currency/exchange_rates.cr
@@ -1,32 +1,49 @@
 require "http/client"
-require "xml"
+require "json"
 
 module Europe
   module Currency
     module ExchangeRates
-      EXCHANGE_URL = "http://www.ecb.europa.eu/stats/" \
-                     "eurofxref/eurofxref-daily.xml"
+      EXCHANGE_URL = "https://floatrates.com/daily/eur.json"
 
       def self.retrieve
         response = HTTP::Client.get(EXCHANGE_URL)
         extract_rates(response.body)
       end
 
-      def self.extract_rates(xml_string : String)
-        data = XML.parse(xml_string)
+      def self.extract_rates(json_string)
+        data = JSON.parse(json_string)
         rates = {
-          date: Time.parse(
-            data.children.first.children[5].children[1].attributes[0].content,
-            "%F", location: Time::Location::UTC),
+          date: parse_time(data["usd"]["date"].to_s),
           rates: {} of String => Float64,
         }
 
-        data.children.first.children[5].children[1].children.select(&.element?).each do |node|
-          next unless node.name == "Cube"
-          rates[:rates][node.attributes[0].content] =
-            node.attributes[1].content.to_f
+        data.as_h.keys.each do |currency|
+          rates[:rates][currency.upcase] =
+          data[currency]["rate"].to_s.to_f
         end
         rates
+      end
+
+      def self.parse_time(time : String)
+        splitted_date = time.split(" ")
+        splitted_time = splitted_date[4].split(":")
+        Time.new(
+          splitted_date[3].to_i32,
+          parse_month(splitted_date[2]).to_i32,
+          splitted_date[1].to_i32,
+          splitted_time[0].to_i32,
+          splitted_time[1].to_i32,
+          splitted_time[2].to_i32,
+          location: Time::Location::UTC
+        )
+      end
+
+      def self.parse_month(month : String)
+        case month
+        when "Nov" then 11
+        else 1
+        end
       end
     end
   end
